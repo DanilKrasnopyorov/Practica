29d59203219e7706fb3e1d201fc1f07dd24fe69d
Игнатьев Даниил <pr118ignatev@oat.local>
2021-11-18 09:57:02 +0600

    add project

---
+++ RealEstateAgencyApp/AddClient.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RealEstateAgencyApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddClient.xaml
+    /// </summary>
+    public partial class AddClient : Window
+    {
+        public AddClient()
+        {
+            InitializeComponent();
+        }
+
+        private void back(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
